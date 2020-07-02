b3774c9619ac68b0c6d5bae614ba6e6323c6fe32
Mindaugas Žvirblis <mz@9g.lt>
2020-07-02 18:06:15 +0300

    initial commit

---
+++ src/SimpleScheduler.h
@@ -0,0 +1,38 @@
+#inclide <stdlib.h>
+
+typedef void(*func_t)();
+
+struct Node {
+    unsigned long time;
+    unsigned long millis;
+    func_t fn;
+    struct Node *next;
+    struct Node *prev;
+};
+
+struct Node *mylist;
+
+
+void schedule_run() {
+    struct Node *n = mylist;
+    while(n) {
+        if(millis() - n->millis >= n->time) {
+            n->fn();
+           mylist->next = n->next;
+           free(n);
+        }
+        n = mylist->next;
+    }
+}
+
+
+void schedule(unsigned long dl, func_t fn) {
+    struct Node *node;
+    node = (struct Node*) malloc(sizeof(struct Node));
+    node->millis = millis();
+    node->time = dl;
+    node->fn = fn;
+    node->next = mylist;
+    mylist = node;
+}
+
