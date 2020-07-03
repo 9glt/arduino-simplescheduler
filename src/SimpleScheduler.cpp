87df62740f158e1081c84b62bc2912df82af68b7
Mindaugas Žvirblis <mz@9g.lt>
2020-07-03 09:54:55 +0300

    cleanup

--- src/SimpleScheduler.cpp
+++ src/SimpleScheduler.cpp
@@ -3,34 +3,25 @@
 
 void __append(Node** head_ref, unsigned long t, unsigned long m, void (*fn)() )  
 {  
-    /* 1. allocate node */
     Node* new_node = (struct Node*) malloc(sizeof(struct Node));
   
-    Node *last = *head_ref; /* used in step 5*/
+    Node *last = *head_ref; 
   
-    /* 2. put in the data */
     new_node->millis = m;  
     new_node->time = t;
     new_node->fn = fn;
   
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/
     new_node->next = NULL;  
   
-    /* 4. If the Linked List is empty, 
-    then make the new node as head */
     if (*head_ref == NULL)  
     {  
         *head_ref = new_node;  
         return;  
     }  
   
-    /* 5. Else traverse till the last node */
     while (last->next != NULL)  
         last = last->next;  
   
-    /* 6. Change the next of last node */
     last->next = new_node;  
     return;  
 }
